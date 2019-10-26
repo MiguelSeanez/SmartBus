22102766fd8534d833e540118c76383d3e8ed9f2
Miguel <jmiguel_rs@hotmail.com>
2019-10-26 16:58:03 -0500

    Mostrar perfil de la BD

--- src/app/components/users/profile/profile.component.ts
+++ src/app/components/users/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { UserInterface } from './../../../models/user';
 import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from 'src/app/services/data-api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private dataApi: DataApiService) { }
 
   //user: UserInterface;
   user: UserInterface ={
+    id: '',
     name:'',
     email:'',
     gender: '',
@@ -21,14 +24,24 @@ export class ProfileComponent implements OnInit {
     balance: 0,
   }
 
+  getUser: Observable<UserInterface>;
+
   public providerId: string = 'null';
 
   ngOnInit() {
     this.authService.isAuth().subscribe(user =>{
       if(user){
-        this.user.name = user.displayName;
-        this.user.email = user.email;
-        this.providerId = user.providerData[0].providerId;
+        this.dataApi.getOneUser(user.uid).subscribe(user => {
+          this.user = user;
+          this.dataApi.selectedUser = Object.assign({}, this.user);
+        });
+        //this.user.id = user.uid;       
+        //this.user.name = user.displayName;
+        //this.user.email = user.email;
+        //this.providerId = user.providerData[0].providerId;
+        //this.user.balance = 10;
+        
+        
         console.log('user', user);
       }
     }); 
